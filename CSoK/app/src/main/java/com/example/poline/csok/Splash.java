cfb4f33bb8dfb58796ec0ba24a42b6d9fcb10c0e
Arthur Pundo <pundoarthur@gmail.com>
2018-03-05 14:17:28 +0300

    initial commit

---
+++ CSoK/app/src/main/java/com/example/poline/csok/Splash.java
@@ -0,0 +1,15 @@
+package com.example.poline.csok;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+    public void SPLASH_TIME_OUT 4000;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
